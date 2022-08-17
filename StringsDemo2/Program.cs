9f492e93e6c19223c10607f9e85e906118c0a26d
akilbek.ismanov <akilbek.ismanov@ANK5-YZLMORT-03>
2022-08-17 10:50:02 +0300

    version1

---
+++ StringsDemo2/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace StringsDemo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // kullanıcıdan pozitif bir tamsayı al
+            // bu sayinın 1000'ler, 100'ler, 10'lar, 1'ler basamaklarını yazdır
+
+            Console.WriteLine("Sayı gir: ");
+            string sayi = Console.ReadLine();
+
+            string[] basamakSayilari = new string[sayi.Length];
+
+            for (int i=0; i <= sayi.Length -1; i++)
+            {
+                basamakSayilari[i] = sayi[i].ToString();
+            }
+        }
+    }
+}
